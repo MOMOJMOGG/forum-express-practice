0c91717fb4207dfffee5984b10bb653983f7e846
MOMOJMOGG <momomacaca@gmail.com>
2021-06-19 17:07:28 +0800

    feat: add admin restaurant put feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -43,6 +43,37 @@ const adminController = {
       return console.warn(err)
     }
   },
+
+  editRestaurant: async (req, res) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
+      return res.render('admin/create', { restaurant: restaurant })
+    } catch (err) {
+      return console.warn(err)
+    }
+  },
+
+  putRestaurant: async (req, res) => {
+    try {
+      if (!req.body.name) {
+        req.flash('error_messages', "name didn't exist")
+        return res.redirect('back')
+      }
+
+      const restaurant = await Restaurant.findByPk(req.params.id)
+      await restaurant.update({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description
+      })
+      req.flash('success_messages', 'restaurant was successfully to update')
+      res.redirect('/admin/restaurants')
+    } catch (err) {
+      return console.warn(err)
+    }
+  },
 }
 
 module.exports = adminController
