2b48b6a26b4ea0c1dffbfb4c39bf570ccfaf1d4f
MOMOJMOGG <momomacaca@gmail.com>
2021-06-19 16:41:00 +0800

    feat: confirm user password and unique email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -8,6 +10,14 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars �
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,25 @@ const userController = {
 
   signUp: async (req, res) => {
     try {
-      await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-      })
-      return res.redirect('/signin')
+      if (req.body.passwordCheck !== req.body.password) {
+        req.flash('error_messages', '兩次密碼輸入不同！')
+        return res.redirect('/signup')
+      } else {
+        const user = await User.findOne({ where: { email: req.body.email } })
+        if (user) {
+          req.flash('error_messages', '信箱重複！')
+          return res.redirect('/signup')
+        } else {
+          await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          })
+
+          req.flash('success_messages', '成功註冊帳號！')
+          return res.redirect('/signin')
+        }
+      }
     } catch (err) {
       return console.warn(err)
     }
