c932d011fc0538b8407235aa940090d59d6b72c1
MOMOJMOGG <momomacaca@gmail.com>
2021-06-19 16:34:37 +0800

    feat: add userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+const userController = {
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  },
+
+  signUp: async (req, res) => {
+    try {
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+      })
+      return res.redirect('/signin')
+    } catch (err) {
+      return console.warn(err)
+    }
+  }
+}
+
+module.exports = userController
