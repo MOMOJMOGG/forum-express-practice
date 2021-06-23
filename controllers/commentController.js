2e6e1b4d466ef2802ef95de0f73e5e10da6a0f64
MOMOJMOGG <momomacaca@gmail.com>
2021-06-23 23:58:09 +0800

    feat: add post comment feature on restaurant page

---
+++ controllers/commentController.js
@@ -0,0 +1,19 @@
+const db = require('../models')
+const Comment = db.Comment
+
+const commentController = {
+  postComment: async (req, res) => {
+    try {
+      await Comment.create({
+        text: req.body.text,
+        RestaurantId: req.body.restaurantId,
+        UserId: req.user.id
+      })
+      return res.redirect(`/restaurants/${req.body.restaurantId}`)
+    } catch (err) {
+      return console.warn(err)
+    }
+  }
+}
+
+module.exports = commentController
