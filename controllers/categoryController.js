6a646327b76251577c0d24e31146e8e4e3269ad3
MOMOJMOGG <momomacaca@gmail.com>
2021-06-23 23:14:00 +0800

    feat: add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,7 +5,12 @@ let categoryController = {
   getCategories: async (req, res) => {
     try {
       const categories = await Category.findAll({ raw: true, nest: true })
-      return res.render('admin/categories', { categories: categories })
+      if (req.params.id) {
+        const category = await Category.findByPk(req.params.id)
+        return res.render('admin/categories', { categories: categories, category: category.toJSON() })
+      } else {
+        return res.render('admin/categories', { categories: categories })
+      }
     } catch (err) {
       return console.warn(err)
     }
@@ -18,6 +23,21 @@ let categoryController = {
         return res.redirect('back')
       } else {
         await Category.create({ name: req.body.name })
+        return res.redirect('/admin/categories')
+      }
+    } catch (err) {
+      return console.warn(err)
+    }
+  },
+
+  putCategory: async (req, res) => {
+    try {
+      if (!req.body.name) {
+        req.flash('error_messages', 'name didn\'t exist')
+        return res.redirect('back')
+      } else {
+        const category = await Category.findByPk(req.params.id)
+        await category.update(req.body)
         return res.redirect('/admin/categories')
       }
     } catch (err) {
