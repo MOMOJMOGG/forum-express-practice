8966fbb6e7f1db3801ba1b000954caa6f47f38c7
MOMOJMOGG <momomacaca@gmail.com>
2021-06-23 23:24:27 +0800

    feat: add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,21 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll({ include: Category })
+      const data = restaurants.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.substring(0, 50), // 當 key 重覆時，會以後面出現的為準
+        categoryName: r.Category.name
+      }))
+      return res.render('restaurants', { restaurants: data })
+    } catch (err) {
+      return console.warn(err)
+    }
   }
 }
+
 module.exports = restController
