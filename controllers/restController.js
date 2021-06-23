b72ea935be04e0a3d800a4af9bfda53f027f254d
MOMOJMOGG <momomacaca@gmail.com>
2021-06-23 23:49:07 +0800

    feat: add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,19 +1,32 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 10
 
 const restController = {
   getRestaurants: async (req, res) => {
     try {
+      let offset = 0
       const whereQuery = {}
       let categoryId = ''
+
+      if (req.query.page) {
+        offset = (req.query.page - 1) * pageLimit
+      }
+
       if (req.query.categoryId) {
         categoryId = Number(req.query.categoryId)
         whereQuery.CategoryId = categoryId
       }
 
-      const restaurants = await Restaurant.findAll({ include: Category, where: whereQuery })
-      const data = restaurants.map(r => ({
+      const results = await Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset: offset, limit: pageLimit })
+      // data for pagination
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(results.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+      const data = results.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50), // 當 key 重覆時，會以後面出現的為準
         categoryName: r.Category.name,
@@ -21,7 +34,10 @@ const restController = {
       }))
       const categories = await Category.findAll({ raw: true, nest: true })
 
-      return res.render('restaurants', { restaurants: data, categories: categories })
+      return res.render('restaurants', {
+        restaurants: data, categories: categories, categoryId: categoryId,
+        page: page, totalPage: totalPage, prev: prev, next: next
+      })
     } catch (err) {
       return console.warn(err)
     }
