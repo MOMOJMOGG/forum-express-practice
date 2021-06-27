543b56ecb2395032387d36878f447f324cf47aac
MOMOJMOGG <momomacaca@gmail.com>
2021-06-27 20:42:50 +0800

    feat: modify seed data

--- seeders/20210626172631-comments-data.js
+++ seeders/20210626172631-comments-data.js
@@ -3,11 +3,11 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments',
-      Array.from({ length: 5 }).map((item, index) => ({
+      Array.from({ length: 10 }).map((item, index) => ({
         id: index * 10 + 1,
         text: faker.lorem.sentence(),
-        // UserId: Math.floor(Math.random() * 3) * 10 + 1,
-        UserId: 1,
+        UserId: Math.floor(Math.random() * 3) * 10 + 1,
+        // UserId: 1,
         RestaurantId: Math.floor(Math.random() * 50) + 1,
         createdAt: new Date(),
         updatedAt: new Date()
